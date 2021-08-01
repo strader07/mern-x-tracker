8b6937b32894585eebeee2e859221f27edb8b02a
rvision0925 <v7sion@topdogtek.com>
2021-07-31 22:20:53 -0400

    frontend & API connection

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -47,7 +47,7 @@ router.route('/update/:id').post((req, res) => {
 			exercise.date = Date.parse(req.body.date) || exercise.date;
 
 			exercise.save()
-				.then(() => res.json('Exercise updated.'))
+				.then(() => res.json(exercise))
 				.catch(err => res.status(400).json('Error: ' + err));
 		})
 		.catch(err => res.status(400).json('Error: ' + err));
